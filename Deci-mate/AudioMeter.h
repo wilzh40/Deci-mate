3c5f827169caf3677f13ba9e7766cef89ca13961
wilzh40 <wilzh40@gmail.com>
2015-07-11 16:35:36 -0700

    I messed up

--- Deci-mate/AudioMeter.h
+++ Deci-mate/AudioMeter.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface AudioMeter : NSObject
-
+- (void) initAudioMeter;
 @end
--- Deci-mate/ViewController.swift
+++ Deci-mate/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        AudioMeter().initAudioMeter()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +20,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
