42fe53544f1ae3c571d67eb988740c9a08962214
wilzh40 <wilzh40@gmail.com>
2015-07-11 15:26:01 -0700

    Initial Commit

---
+++ Deci-mateTests/Deci_mateTests.swift
@@ -0,0 +1,36 @@
+//
+//  Deci_mateTests.swift
+//  Deci-mateTests
+//
+//  Created by Wilson Zhao on 7/11/15.
+//  Copyright (c) 2015 Innogen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Deci_mateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
